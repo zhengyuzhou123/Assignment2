f1137a649fa7a12f3f66925efede3e3f48b74b54
zhengyuzhou123 <231106004+zhengyuzhou123@users.noreply.github.com>
2025-09-29 02:39:18 +0000

    Add script to restructure raw dataset into relational tables

---
+++ dataframe.py
@@ -0,0 +1,58 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import monotonically_increasing_id
+
+# Start a Spark session
+spark = SparkSession.builder.appName("RelationalTables").getOrCreate()
+
+# Load the CSV file into a Spark DataFrame
+df = spark.read.option("header", "true").option("inferSchema",
+"true").csv("data/lightcast_job_postings.csv")
+
+#Company DataFrame
+companies = df.select(
+    df["COMPANY"].alias("COMPANY_ID"),
+    "COMPANY",
+    "COMPANY_NAME",
+    "COMPANY_RAW",
+    "COMPANY_IS_STAFFING"
+).dropDuplicates(["COMPANY_ID"])
+
+companies.createOrReplaceTempView("companies")
+
+
+#Location DataFrame
+locations = df.select(
+    df["LOCATION"].alias("LOCATION_ID"),
+    "CITY_NAME",
+    "STATE_NAME",
+    "COUNTY_NAME",
+    "MSA",
+    "MSA_NAME"
+).dropDuplicates(["LOCATION_ID"])
+locations.createOrReplaceTempView("locations")
+
+#Job DataFrame
+job_postings= df.select(
+    "ID",
+    "TITLE_CLEAN",
+    df["COMPANY"].alias("COMPANY_ID"),
+    df["NAICS_2022_6"].alias("INDUSTRY_ID"),
+    "EMPLOYMENT_TYPE_NAME",
+    "REMOTE_TYPE_NAME",
+    "BODY",
+    "MIN_YEARS_EXPERIENCE",
+    "MAX_YEARS_EXPERIENCE",
+    "SALARY",
+    "SALARY_FROM",
+    "SALARY_TO",
+    df["LOCATION"].alias("LOCATION_ID"),
+    "POSTED", 
+    "EXPIRED", 
+    "DURATION"
+)
+job_postings.createOrReplaceTempView("job_postings")
+
+print("Companies row:", companies.count())
+print("Locations row:", locations.count())
+print("Job Postings row:", job_postings.count())
+
